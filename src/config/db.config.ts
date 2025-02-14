95f6e82e31890a0bdf6dfbf417269be5c2c4d2a9
shivam <107310556+shivamshukla13jun@users.noreply.github.com>
2025-02-15 01:42:26 +0530

    refactor: update MongoDB URI handling for development and production; improve database connection logic in db.config.ts

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,15 +2,15 @@ import mongoose from "mongoose";
 import { MONGO_DB_URI } from ".";
 import { createDefaultAdmin } from "@/utils/createDefaultAdmin";
 
-
 const connectToDb = async () => {
   try {
-    const connect = await mongoose.connect(MONGO_DB_URI as string);
+    const mongouri=process.env.npm_lifecycle_event === "dev" ? process.env.DEV_MONGO_DB_URI : process.env.MONGO_DB_URI
+    const connect = await mongoose.connect(mongouri as string);
      await createDefaultAdmin()
     return connect;
   } catch (error) {
     console.log(error);
-    // process.exit(1);
+    process.exit(1);
   }
 };
 
